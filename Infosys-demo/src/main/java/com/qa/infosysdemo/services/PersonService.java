5792e634d32513d309c1cd6ba82fabecde9bb75b
M-java86 <mpythondata@outlook.com>
2021-06-10 10:49:27 -0400

    MO: addPerson, getAllPeople and updatePerson

--- Infosys-demo/src/main/java/com/qa/infosysdemo/services/PersonService.java
+++ Infosys-demo/src/main/java/com/qa/infosysdemo/services/PersonService.java
@@ -9,6 +9,24 @@ import org.springframework.stereotype.Service;
 public class PersonService {
 	
 	private List<Person> people = new ArrayList<> ();
+	
+	public Person addPerson(Person person) {
+		// Add new Person
+		this.people.add(person);
+		//return last added Person from List 
+		return this.people.get(this.people.size()  -1);
+		
+	}
+	
+	public List<Person> getAllPeople() {
+		//return this whole list 
+		return this.people;
+	}
+	
+	public Person updatePerson(int id, Person person) {
+		//Remove 
+		
+	}
 
 
 }
