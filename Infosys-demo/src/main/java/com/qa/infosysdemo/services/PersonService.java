46951cebb1e2619100e66e5dafb87aaf8dbf9fe0
M-java86 <mpythondata@outlook.com>
2021-06-10 10:36:50 -0400

    MO: imported ArrayList and java List

--- Infosys-demo/src/main/java/com/qa/infosysdemo/services/PersonService.java
+++ Infosys-demo/src/main/java/com/qa/infosysdemo/services/PersonService.java
@@ -1,10 +1,14 @@
 package com.qa.infosysdemo.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 	
+	private List<Person> people = new ArrayList<> ();
 
 
 }
